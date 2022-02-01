e01388f1cb9bd054961f10564c091ea8c1edaf11
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 20:18:56 +0530

    [Rakshit] Add: Swap 2 no program added

---
+++ BasicCoreAndFunctional/SwapNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreAndFunctional
+{
+    public class SwapNumber
+    {
+        public void Swap()
+        {
+            Console.WriteLine("Enter the first number: ");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number: ");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Numbers after swapping: ");
+
+            firstNumber += secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber -= secondNumber;
+
+            Console.WriteLine("First no = " + firstNumber + "\nSecond no = " + secondNumber);
+        }
+    }
+}
