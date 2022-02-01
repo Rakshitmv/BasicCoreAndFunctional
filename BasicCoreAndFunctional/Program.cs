f49f6527153316716fecc2f11cb2b73ed9fff877
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 18:55:54 +0530

    [Rakshit] Add: FilpCoin program added

---
+++ BasicCoreAndFunctional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreAndFunctional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter 1 to Flip a Coin\n" + "Enter 2 to exit\n");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin flipCoin = new FlipCoin();
+                        flipCoin.Toss();
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+
+                }
+            }
+        }
+    }
+}
