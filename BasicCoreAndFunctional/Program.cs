73db00425c294fec292b32ba8365e2ac1eb9ad7c
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 19:14:07 +0530

    [Rakshit] Add: Power of 2 program added

--- BasicCoreAndFunctional/Program.cs
+++ BasicCoreAndFunctional/Program.cs
@@ -14,7 +14,7 @@ namespace BasicCoreAndFunctional
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter 1: To Flip a Coin\n" + "Enter 2: To Check For leap Year\n" + "Enter 3: To exit\n");
+                Console.WriteLine("Enter 1: To Flip a Coin\n" + "Enter 2: To Check For leap Year\n" + "Enter 3: To power of 2\n" + "Enter 4: To exit\n");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -27,6 +27,10 @@ namespace BasicCoreAndFunctional
                         leapYear.Leap();
                         break;
                     case 3:
+                        PowerOf2 powerOf2 = new PowerOf2();
+                        powerOf2.Power2();
+                        break;
+                    case 4:
                         flag = false;
                         break;
 
