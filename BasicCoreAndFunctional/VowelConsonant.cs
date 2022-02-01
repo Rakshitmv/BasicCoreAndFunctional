2d0a5cb6db50664beb41d881be4ac518cbb32f8b
Rakshitmv <rakshit8976646711@gmail.com>
2022-02-01 20:41:55 +0530

    [Rakshit] Add: Check Vowel Consonant program added

---
+++ BasicCoreAndFunctional/VowelConsonant.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreAndFunctional
+{
+    public class VowelConsonant
+    {
+        public void CheckVowelConsonant()
+        {
+            Console.WriteLine("Enter any character/alphabet: ");
+            char alphabet = Convert.ToChar(Console.ReadLine());
+
+            if (alphabet == 'a' || alphabet == 'A' || alphabet == 'e' || alphabet == 'E' || alphabet == 'i' || alphabet == 'I' || alphabet == 'o' || alphabet == 'O' || alphabet == 'u' || alphabet == 'U')
+            {
+                Console.WriteLine($"The entered alphabet {alphabet} is a Vowel");
+            }
+            else
+            {
+                Console.WriteLine($"The entered alphabet {alphabet} is a Consonant");
+            }
+        }
+    }
+}
+
+      
+    
